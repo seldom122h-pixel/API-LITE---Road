6f51d06a86045d28f67d8924ab0996e3fca86366
MAUNT <seldom122h@gmail.com>
2026-06-13 15:04:15 +0300

    + New project

---
+++ Python Projects/CircleCalculator.py
@@ -0,0 +1,19 @@
+import math
+
+while True:
+    try:
+        print('----------------------')
+        radius = float(input('Enter circle radius: '))
+        break
+    except ValueError:
+        print(f"Enter a valid value!")
+        continue
+
+CircleC = 2 * math.pi * radius
+CircleS = math.pi * pow(radius, 2)
+
+print('----------------------')
+print(f"Circle lenght: {round(CircleC, 2)}")
+print(f"Сircle area: {round(CircleS, 2)}")
+print('----------------------')
+
